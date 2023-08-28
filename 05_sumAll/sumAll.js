fa9dc1cdff4d2b2bc0f7d98ab6d401d57232aa19
Kevin Tran <91237886+Kevonosdiaz@users.noreply.github.com>
2023-08-28 17:09:15 -0600

    Exercises 05-07 Complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    res = 0;
 
+    // Return an error for non-integers or negative numbers
+    if (!Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return "ERROR";
+    }
+
+    // Switch the numbers so that x is the smaller num
+    if (x > y) {
+        let temp = x;
+        x = y;
+        y = temp;
+    }
+
+    for (x; x <= y; x++) {
+        res += x;
+    }
+    return res;
 };
 
 // Do not edit below this line
